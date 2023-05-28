227c2f3a50f96d5d31624273cf173fa76af70190
RaimisReej <54449879+RaimisReej@users.noreply.github.com>
2023-05-28 17:55:17 +0100

    individList start

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,13 +1,18 @@
 import prisma from "@/app/libs/prismadb"
 
 export default async function getListings() {
-    try{
+    try {
         const listings = await prisma.listing.findMany({
             orderBy: {
                 createAt: 'desc'
             }
         })
-        return listings;
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createAt: listing.createAt.toISOString()
+        }))
+
+        return safeListings;
 
     } catch(error: any) {
         throw new Error(error);
---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,7 @@
+const ListingPage = () => {
+    return ( 
+        <div className="">My individual listing page</div>
+     );
+}
+ 
+export default ListingPage;
